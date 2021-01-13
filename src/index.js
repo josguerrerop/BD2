a359c4df6990515f3e33830018b97d6e11d85540
miller barahona <millermiller01@hotmail.com>
2021-01-13 18:41:20 -0500

    formulario de mueble

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const flash = require('connect-flash');
 const session = require('express-session');
 const pgs = require('connect-pg-simple')(session);
-const passport =require('passport');
+const passport = require('passport');
 
 
 //inicio
@@ -14,49 +14,50 @@ require('./lib/passport');
 
 
 // Settings
-app.set('port', process.env.PORT|| 4000);
-app.set('views',path.join(__dirname,'views'));
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
 
-app.engine('.hbs',exphbs({
+app.engine('.hbs', exphbs({
     defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'),'layouts'),
-    partialsDir: path.join(app.get('views'),'partials'),
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs',
     helpers: require('./lib/handlebars')
 }));
-app.set('view engine','.hbs');
+app.set('view engine', '.hbs');
 
-  
+
 //midleware
 app.use(session({
     store: new pgs({
-        conString: "postgres://postgres:x@localhost:5432/prontomueble"}),
+        conString: "postgres://postgres:x@localhost:8080/prontomueble"
+    }),
     secret: 'x',
     resave: false,
     saveUninitialized: true
-  }));
+}));
 
 app.use(flash());
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 // Global variables
 
-app.use((req,res,next)=>{
-app.locals.c = req.flash('c');
-next();
+app.use((req, res, next) => {
+    app.locals.c = req.flash('c');
+    next();
 });
 
 // Routes
 app.use(require('./routes'));
 app.use(require('./routes/authentication'));
-app.use('/links',require('./routes/links'));
+app.use('/links', require('./routes/links'));
 // Public
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Starting server
-app.listen(app.get('port'), ()=> {
-console.log('Server on port' , app.get('port'));
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
 });
--- src/routes/links.js
+++ src/routes/links.js
@@ -7,41 +7,44 @@ const db = require('../database');
 
 
 
- router.get('/take', async (req,res) => {
-  try{  
-  var x = await db.query('select * from material');
-  var proveedores = await db.query('select id from color');
-  res.render('links/take',{x,proveedores});
-   }catch(error){
-     console.log(error);
-  }
+router.get('/take', async(req, res) => {
+    try {
+        var material = await db.query('select * from material');
+        const color = await db.query('select * from color;');
+        const tipo = await db.query('select * from tipo_mueble');
+        const vendedor = await db.query('select * from vendedor');
+        const proveedor = await db.query('select * from proveedor');
+        res.render('links/take', { material, color, tipo, vendedor, proveedor });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
-router.get('/add',(req,res)=>{
-res.render('links/add');
+router.get('/add', (req, res) => {
+    res.render('links/add');
 });
 
-router.get('/xd',async (req,res)=>{
-  req.flash('c','xddd');
-  req.session.save(function() {
-    res.redirect('/links/take');
-});
-
-});
-
-
-router.post('/add', async (req,res)=>{
-const {id,name} =req.body;
-const link = {id,name};
-try{  
-    await db.query('INSERT INTO material (id,material) values (${id},${name})',link); 
-      req.flash('c','xddd');
-      req.session.save(function() {
-      res.redirect('/links/take');
+router.get('/xd', async(req, res) => {
+    req.flash('c', 'xddd');
+    req.session.save(function() {
+        res.redirect('/links/take');
     });
-   }catch(error){
-    console.log(error);
-  }
+
+});
+
+
+router.post('/add', async(req, res) => {
+    const { id, name } = req.body;
+    const link = { id, name };
+    try {
+        await db.query('INSERT INTO material (id,material) values (${id},${name})', link);
+        req.flash('c', 'xddd');
+        req.session.save(function() {
+            res.redirect('/links/take');
+        });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 module.exports = router;
