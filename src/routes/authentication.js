9509dec351c66bba03094904d065e8d64c364678
JOHN SEBASTIAN GUERRERO PINTO <josguerrerop@correo.udistrital.edu.co>
2021-02-19 18:32:07 -0500

    ok

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,26 +2,33 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.get('/signup', async (req,res) => {
-res.render('auth/singup');
+router.get('/signup', async(req, res) => {
+    res.render('auth/singup');
 });
 
 router.post('/signup', passport.authenticate('local.signup', {
     successRedirect: '/links/home',
     failureRedirect: '/signup',
     failureFlash: true
-  }));
-  
+}));
+
 
 
 router.get('/signin', (req, res) => {
-  res.render('auth/signin');
+    res.render('auth/signin');
 });
 
 router.post('/signin', passport.authenticate('local.signin', {
-  successRedirect: '/links/home',
-  failureRedirect: '/signup',
-  failureFlash: true
+    successRedirect: '/links/home',
+    failureRedirect: '/signup',
+    failureFlash: true
 }));
+
+
+router.get('/logout', (req, res) => {
+    req.logOut();
+    res.redirect('/');
+});
+
 
 module.exports = router;
